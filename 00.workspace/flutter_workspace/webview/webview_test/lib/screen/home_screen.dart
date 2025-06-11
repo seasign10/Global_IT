7e126999672d9588649dac16090e807eaa7a15ce
seasign10 <seasign10@gmail.com>
2025-06-11 13:33:02 +0900

    250611

--- 00.workspace/flutter_workspace/webview/webview_test/lib/screen/home_screen.dart
+++ 00.workspace/flutter_workspace/webview/webview_test/lib/screen/home_screen.dart
@@ -2,28 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller; // ❶ 컨트롤러 변수 생성
+  WebViewController? controller; // 컨트롤러 변수 생성
 
   HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( // ➊ 앱바 위젯 추가
-        // ➋ 배경색 지정
+      appBar: AppBar( // 앱바 위젯 추가
+        // 배경색 지정
         backgroundColor: Colors.blue,
-        // ➌ 앱 타이틀 설정
+        // 앱 타이틀 설정
         title: Text('Board'),
-        // ➍ 가운데 정렬
+        // 가운데 정렬
         centerTitle: true,
         actions: [
           IconButton(
 
-            // ➋ 눌렀을 때 콜백 함수 설정
+            // 눌렀을 때 콜백 함수 설정
             onPressed: () {
               if (controller != null) {
 
-                // ➌ 웹뷰에서 보여줄 사이트 실행하기
+                // 웹뷰에서 보여줄 사이트 실행하기
                 // controller!.loadUrl('https://blog.codefactory.ai');
                 controller!.loadUrl('http://10.0.2.2:8282/board/list');
               }
@@ -36,12 +36,12 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: WebView(  // ❷ WebView 추가하기
+      body: WebView(  // WebView 추가하기
         // initialUrl: 'https://blog.codefactory.ai',
         initialUrl: 'http://10.0.2.2:8282/board/list',
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (WebViewController controller) {
-          this.controller = controller; // ➍ 위젯에 컨트롤러 저장
+          this.controller = controller; // 위젯에 컨트롤러 저장
         },
       ),
     );
